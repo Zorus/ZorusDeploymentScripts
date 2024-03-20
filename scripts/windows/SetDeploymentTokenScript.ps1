8e2e31a7947ba84683fb3aee87b18d45597ef94e
John Carnevalla <jcarnevalla@zorustech.com>
2024-03-20 14:20:43 -0400

    chore: updated installation path retrieval logic. ticket: AAW-1094

--- scripts/windows/SetDeploymentTokenScript.ps1
+++ scripts/windows/SetDeploymentTokenScript.ps1
@@ -6,25 +6,28 @@ if ([string]::IsNullOrEmpty($Token))
     Exit
 }
 
-# get stored deployment token value
 $hklm = [Microsoft.Win32.RegistryKey]::OpenBaseKey([Microsoft.Win32.RegistryHive]::LocalMachine, [Microsoft.Win32.RegistryView]::Registry32)
 $key =  $hklm.OpenSubKey("SOFTWARE\Zorus Inc.\Archon Agent")
+
+# get stored deployment token value
 $previousToken = $key.GetValue("deploymentKey")
 
 # get installation path
-$path = Get-ItemProperty -Path $zorusRegistry -Name Path 2>$null | Select-Object -ExpandProperty Path
-
+$path = $key.GetValue("Path")
 if (!$path)
 {
     Write-Host "Custom installation path not found, using default location."
-    $path = "C:\Program Files\Zorus Inc\Archon Agent\"
+    $path = "C:\Program Files\Zorus Inc\Archon Agent"
+}
+else
+{
+    $path = $path.TrimEnd("\")
 }
 
 Start-Process -FilePath "$path\Zorus Deployment Agent\ZorusDeploymentAgent.exe" -ArgumentList "--token=$Token" -Wait
 
 # get stored deployment token value
 $updatedToken = $key.GetValue("deploymentKey")
-
 if ($previousToken -eq $updatedToken)
 {
     Write-Host "Failed to update deployment token. Check your token and try again."
@@ -32,4 +35,3 @@ if ($previousToken -eq $updatedToken)
 }
 
 Write-Host "Deployment token updated successfully."
-
